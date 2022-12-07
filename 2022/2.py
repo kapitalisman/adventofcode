9d6a274128927985d6e00258960147673f90ccbe
kapitalisman <44622275+kapitalisman@users.noreply.github.com>
2022-12-07 14:28:11 +0100

    update requirements

--- 2022/2.py
+++ 2022/2.py
@@ -12,45 +12,45 @@ def score(func):
 
 def p1(line):
     match line:
-        case "A X":
+        case 'A X':
             return 3 + 1
-        case "A Y":
+        case 'A Y':
             return 6 + 2
-        case "A Z":
+        case 'A Z':
             return 0 + 3
-        case "B X":
+        case 'B X':
             return 0 + 1
-        case "B Y":
+        case 'B Y':
             return 3 + 2
-        case "B Z":
+        case 'B Z':
             return 6 + 3
-        case "C X":
+        case 'C X':
             return 6 + 1
-        case "C Y":
+        case 'C Y':
             return 0 + 2
-        case "C Z":
+        case 'C Z':
             return 3 + 3
 
 
 def p2(line):
     match line:
-        case "A X":
+        case 'A X':
             return 0 + 3
-        case "A Y":
+        case 'A Y':
             return 3 + 1
-        case "A Z":
+        case 'A Z':
             return 6 + 2
-        case "B X":
+        case 'B X':
             return 0 + 1
-        case "B Y":
+        case 'B Y':
             return 3 + 2
-        case "B Z":
+        case 'B Z':
             return 6 + 3
-        case "C X":
+        case 'C X':
             return 0 + 2
-        case "C Y":
+        case 'C Y':
             return 3 + 3
-        case "C Z":
+        case 'C Z':
             return 6 + 1
 
 
--- 2022/5.py
+++ 2022/5.py
@@ -9,7 +9,7 @@ def init():
     for line in crates.splitlines():
         index = 0
         for i in list(range(1, len(line), 4)):
-            if line[i] != " ":
+            if line[i] != ' ':
                 stacks[index].append(line[i])
             index += 1
 
@@ -22,12 +22,12 @@ def init():
 
 def p(stacks, by_one=False):
     for proc in procedure.splitlines():
-        num, fr, to = map(int, re.findall(r"\d+", proc))
+        num, fr, to = map(int, re.findall(r'\d+', proc))
         for j in range(num):
             stacks[to - 1].append(stacks[fr -
                                   1].pop(-1 if by_one else j - num))
     return stacks
 
 
-submit("".join(stack[-1] for stack in p(init(), True)))  # p1
-submit("".join(stack[-1] for stack in p(init())))  # p2
+submit(''.join(stack[-1] for stack in p(init(), True)))  # p1
+submit(''.join(stack[-1] for stack in p(init())))  # p2
