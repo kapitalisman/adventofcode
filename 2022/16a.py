e4f0f251561b3ce8853b3bcf742ce19d911cd5ad
kapitalisman <44622275+kapitalisman@users.noreply.github.com>
2022-12-18 21:25:35 +0100

    day 16 part 1

---
+++ 2022/16a.py
@@ -0,0 +1,50 @@
+import random
+import copy
+
+from aocd import data, submit, numbers
+lines = data.splitlines()
+
+valves_start = dict()
+for line in lines:
+    key = line[6:8]
+    rate = int(line.split('=')[1].split(';')[0])
+    tunnels = [x[-2:] for x in line.split(', ')]
+    closed = True
+    valves_start[key] = [rate, tunnels, closed]
+
+
+def monte_carlo():
+    t = 1
+    T = 30
+    release = 0
+    release_incr = 0
+    valve = 'AA'
+    valves = copy.deepcopy(valves_start)
+
+    while t <= T:
+        candidates = [x for x in valves[valve][1] if valves[x][0] >= 10 and valves[x][2] == True]
+        if candidates:
+            valve = random.choice(candidates)
+        else:
+            valve = random.choice(valves[valve][1])
+        release += release_incr
+        if t >= T:
+            break
+        t += 1
+        if valves[valve][2] == False or valves[valve][0] == 0:
+            continue  # do not open valve
+
+        if random.choice([True, False]) or valves[valve][0] >= 10:
+            release += release_incr
+            t += 1
+            valves[valve][2] = False
+            release_incr += valves[valve][0]
+
+    return release
+
+
+results = set()
+for _ in range(1_000_000):
+    results.add(monte_carlo())
+
+print(p1 := max(results))
